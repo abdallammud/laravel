ab95f3d59f436d603be468d909ff9dfc57615e34
= <=>
2024-08-21 18:58:31 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+	<x-partials.topbar></x-partials.topbar>
+	<x-partials.sidebar></x-partials.sidebar>
+	<x-main>
+		<!-- Home content -->
+	</x-main>
+	<x-partials.footer></x-partials.footer>
+</x-layout>
